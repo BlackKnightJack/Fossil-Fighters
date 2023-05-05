1185d0fe3cc171020cb2fb881a0cd9435b953173
BlackKnightJack <35509395+BlackKnightJack@users.noreply.github.com>
2023-05-05 16:27:59 -0700

    Added cutscenes

--- Assets/Scripts/EventPlayer.cs
+++ Assets/Scripts/EventPlayer.cs
@@ -21,39 +21,102 @@ public class EventPlayer : MonoBehaviour
     //Textbox components for when they are necessary
     [Header("Dialog Box")]
     public TMP_Text dialogText;
+    public float dialogLag;
+    GameObject dialogBack;
+    int activeDialog;
+    int lineIndex;
 
     //Dialogs this event contains
-    [Space(10)]
     public Dialog[] dialogs;
 
+    //Flags
+    [Header("Flags")]
+    public bool disposeOnComplete;
+
     //Misc variables
+    ThirdPersonController player;
+    PlayableDirector cutscene;
+    PlayableGraph playingCutscene;
     Camera mainCamera;
-    bool inTriggerRange;
     int layerMask;
+    bool active;
+    bool inTriggerRange;
 
     // Awake triggers at the start of a scene
     void Awake()
     {
         //Cache variables
+        if (dialogText != null) { dialogBack = dialogText.transform.parent.gameObject; }
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<ThirdPersonController>();
         mainCamera = Camera.main;
 
         //Assign layer mask
         layerMask = 1 << LayerMask.NameToLayer("Range");
         layerMask = ~layerMask;
+
+        //Catalog the timeline and activate it if it is set to play automatically
+        if (TryGetComponent(out cutscene) && activationMode == ActivationMode.Auto)
+        {
+            ActivateEvent();
+        }
     }
 
     // Update triggers every frame
     void Update()
     {
-        //In the case that it's an interact activation
-        if (Input.GetMouseButtonDown(0) && inTriggerRange)
+        //If it's not active and the player isn't in another event, look for opportunities to activate it
+        if (!active && player.isActiveAndEnabled)
         {
-            if (Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, layerMask))
+            //In the case that it's an interact activation
+            if (Input.GetMouseButtonDown(0) && inTriggerRange)
             {
-                if (hit.collider == GetComponent<Collider>())
+                if (Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, Mathf.Infinity, layerMask))
                 {
-                    print("In trigger range");
+                    if (hit.collider == GetComponent<Collider>())
+                    {
+                        ActivateEvent();
+                    }
                 }
+            }
+        }
+
+        //Otherwise try to deactivate it
+        else if (active)
+        {
+            //If the message back is active, play dialog
+            if (dialogBack != null && dialogBack.activeSelf)
+            {
+                //Move to next line
+                if (Input.GetMouseButtonDown(0) && dialogText.text == dialogs[activeDialog].lines[lineIndex])
+                {
+                    //If there's another line to move to, increment the line index and start that line
+                    if (lineIndex + 1 < dialogs[activeDialog].lines.Length)
+                    {
+                        lineIndex++;
+                        StartCoroutine(ScrollDialog());
+                    }
+                    else
+                    {
+                        //If a timeline is present, simply resume the timeline
+                        if (cutscene != null)
+                        {
+                            dialogBack.SetActive(false);
+                            cutscene.Resume();
+                        }
+
+                        //If a timeline isn't present, though, deactivate the event full-stop
+                        else
+                        {
+                            DeactivateEvent();
+                        }
+                    }
+                }
+            }
+
+            //If a timeline is present and it's done, deactivate the event
+            if (cutscene != null && !cutscene.playableGraph.IsValid())
+            {
+                DeactivateEvent();
             }
         }
     }
@@ -62,5 +125,94 @@ public class EventPlayer : MonoBehaviour
     public void TriggerRangeChange(bool change)
     {
         inTriggerRange = change;
+    }
+
+    public void OpenDialog(int index)
+    {
+        //Pause the timeline that called this function
+        cutscene.Pause();
+
+        //Open dialog box
+        dialogBack.SetActive(true);
+
+        //Reset dialog variables just to be sure
+        dialogText.text = string.Empty;
+        activeDialog = index;
+        lineIndex = 0;
+
+        //Start the dialog
+        StartCoroutine(ScrollDialog());
+    }
+
+    //Private functions
+    void ActivateEvent()
+    {
+        //First set the event to active
+        active = true;
+
+        //First deactivate the player script
+        player.enabled = false;
+
+        //Attempt to execute a playable
+        if (cutscene != null)
+        {
+            cutscene.Play(cutscene.playableAsset);
+        }
+
+        //If that doesn't work, attempt to display dialog
+        else if (dialogs.Length > 0)
+        {
+            //Face the player towards the event source since it's just regular dialog
+            Vector3 playerEulers = player.transform.eulerAngles;
+            player.transform.LookAt(transform);
+            player.transform.eulerAngles = new Vector3(playerEulers.x, player.transform.eulerAngles.y, playerEulers.z);
+
+            //I might want to swing an attached event camera to face between the event source and the player later
+
+            //Open dialog box
+            dialogBack.SetActive(true);
+
+            //Reset dialog variables just to be sure
+            activeDialog = 0;
+            lineIndex = 0;
+
+            //Start the dialog
+            StartCoroutine(ScrollDialog());
+        }
+
+        //If that doesn't work, attempt to manipulate inventory
+
+        //If that doesn't work, deactivate the event
+    }
+
+    void DeactivateEvent()
+    {
+        //Deactivate the event 
+        active = false;
+
+        //Hide the textbox just to be sure
+        dialogBack.SetActive(false);
+
+        //Give control back to the player character
+        player.enabled = true;
+
+        //Dispose of the event if it is flagged to do so upon deactivation
+        if (disposeOnComplete)
+        {
+            Destroy(this);
+        }
+    }
+
+    IEnumerator ScrollDialog()
+    {
+        //Reset dialog box
+        dialogText.text = string.Empty;
+
+        //Increment the line into the box
+        foreach(char c in dialogs[activeDialog].lines[lineIndex])
+        {
+            dialogText.text += c;
+            yield return new WaitForSeconds(dialogLag);
+        }
     }
 }
